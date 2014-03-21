ce45f032fe36fd6bd37bb8c763f7ca221c75910d
Sudheer Alladi <sudheer.alladi@gmail.com>
2014-03-21 15:22:39 +0000

    Completed exercise-6.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }   # similar to before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i   # ( Added more complex regex to reject email "foo@barr..com")
 	validates :email, presence: true, 
 					  format: { with: VALID_EMAIL_REGEX }, 
 					  uniqueness: { case_sensitive: false }
